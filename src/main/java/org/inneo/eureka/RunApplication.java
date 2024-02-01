380afca5f57d39d4c0d0415f9fda8fe795cd2598
Eduardo Cruz <tecnologiaesolucoespr@gmail.com>
2024-02-01 15:26:57 -0300

    first commit

---
+++ src/main/java/org/inneo/eureka/RunApplication.java
@@ -0,0 +1,15 @@
+package org.inneo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunApplication.class, args);
+	}
+
+}
